dcf87bf3f136e2b2f6ea96bfcb2ecc963bcefdcf
spiegelj <38665502+spiegelj@users.noreply.github.com>
2020-03-02 17:31:06 -0700

    Simple placeholder project
    Only four stub methods exist.  More for completeness of principle than function.

---
+++ Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Logging
+{
+    public static class Log
+    {
+        public static void ToDB(string message)
+        { }
+
+        public static void ToFile()
+        { }
+
+        public static void ToEventLog()
+        { }
+
+        // Placeholder to alert vs. simply track
+        public static void Message()
+        { }
+    }
+}
